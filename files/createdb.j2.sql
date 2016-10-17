e5d61c129ce25be552a54906e369bc2454eabd37
Chris Houseknecht <chousekn@redhat.com>
2016-10-17 16:16:56 -0400

    Fix create user

--- files/createdb.j2.sql
+++ files/createdb.j2.sql
@@ -4,5 +4,5 @@ CREATE DATABASE '{{ postgres_db }}';
 {% if postgres_user == 'postgres' %}
 ALTER USER postgres with SUPERUSER {{ postgres_pass }};
 {% else %}
-CREATE USER {{ postgres_user }} with SUPERUSER {{ postgres_pass }};
+CREATE USER "{{ postgres_user }}" WITH SUPERUSER PASSWORD '{{ postgres_pass }}';
 {% endif %}
