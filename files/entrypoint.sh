df71aff913a4e48611b17fe7ae320e924c92e6ec
Chris Houseknecht <chousekn@redhat.com>
2016-12-24 21:04:56 -0500

    Update passwd file.

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash 
 set -x
 
+function generate_passwd_file() {
+  export USER_ID=$(id -u)
+  export GROUP_ID=$(id -g)
+  grep -v ^postgres /etc/passwd > "$HOME/passwd"
+  echo "postgres:x:${USER_ID}:${GROUP_ID}:PostgreSQL Server:${HOME}:/bin/bash" >> "$HOME/passwd"
+  export LD_PRELOAD=libnss_wrapper.so
+  export NSS_WRAPPER_PASSWD=${HOME}/passwd
+  export NSS_WRAPPER_GROUP=/etc/group
+}
+
 export HOME=/var/lib/pgsql
+
+generate_passwd_file
 
 if [ ! -f ${PGDATA}/pg_hba.conf ]; then
     # init the database
